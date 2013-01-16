b41dda1201d95034a80c1503bf77b7b72a3f7afa
John Biddiscombe <biddisco@cscs.ch>
2013-01-17 00:21:33 +0100

    HDF5 files and REALLOC BUG
    Change-Id: If7be06bf20053c71775dc88c647563c718afe681

--- ThirdParty/hdf5/module.cmake
+++ ThirdParty/hdf5/module.cmake
@@ -1,6 +1,14 @@
 
 if(VTK_USE_SYSTEM_HDF5)
-  if (HDF5_ENABLE_PARALLEL)
+  #-----------------------------------------------
+  # Check if HDF5 was compiled with Parallel support
+  #-----------------------------------------------
+  INCLUDE (${CMAKE_ROOT}/Modules/CheckSymbolExists.cmake)
+  UNSET(HAVE_H5_HAVE_PARALLEL CACHE)
+  SET(CMAKE_REQUIRED_INCLUDES ${HDF5_INCLUDE_DIR} ${VTK_INCLUDE_DIRS})
+  CHECK_SYMBOL_EXISTS(H5_HAVE_PARALLEL "H5pubconf.h" HAVE_H5_HAVE_PARALLEL)
+
+  if (HAVE_H5_HAVE_PARALLEL)
     if (NOT PARAVIEW_USE_MPI)
       MESSAGE(ERROR "System HDF5 uses MPI but PARAVIEW_USE_MPI is ${PARAVIEW_USE_MPI}")
     endif(NOT PARAVIEW_USE_MPI)
@@ -8,7 +16,9 @@ if(VTK_USE_SYSTEM_HDF5)
        # In here we need to say conditionally depend on some kind of find_package(MPI)
        #
        set(vtkhdf5_SYSTEM_INCLUDE_DIRS "${vtkhdf5_SYSTEM_INCLUDE_DIRS};${MPI_C_INCLUDE_PATH}")
-  endif (HDF5_ENABLE_PARALLEL)
+  endif (HAVE_H5_HAVE_PARALLEL)
+
+  include_directories(${MPI_INCLUDE_DIR})
 endif(VTK_USE_SYSTEM_HDF5)
 
 vtk_module(vtkhdf5
