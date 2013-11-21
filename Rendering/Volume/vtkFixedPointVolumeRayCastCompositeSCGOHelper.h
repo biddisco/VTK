46e05933740c3884b8c111e3fbcd34a6c0d1ef05
nwaldin <nwaldin@cscs.ch>
2013-11-21 11:39:41 +0100

    new helper classes for raycaster with regards to 2d transfer function

---
+++ Rendering/Volume/vtkFixedPointVolumeRayCastCompositeSCGOHelper.h
@@ -0,0 +1,61 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkFixedPointVolumeRayCastCompositeSCGOHelper.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+// .NAME vtkFixedPointVolumeRayCastCompositeSCGOHelper - A helper that generates composite images for the volume ray cast mapper
+// .SECTION Description
+// This is one of the helper classes for the vtkFixedPointVolumeRayCastMapper.
+// It will generate composite images using an alpha blending operation.
+// This class should not be used directly, it is a helper class for
+// the mapper and has no user-level API.
+//
+// .SECTION see also
+// vtkFixedPointVolumeRayCastMapper
+
+#ifndef __vtkFixedPointVolumeRayCastCompositeSCGOHelper_h
+#define __vtkFixedPointVolumeRayCastCompositeSCGOHelper_h
+
+#include "vtkRenderingVolumeModule.h" // For export macro
+#include "vtkFixedPointVolumeRayCastHelper.h"
+
+class vtkFixedPointVolumeRayCastMapper;
+class vtkVolume;
+
+class VTKRENDERINGVOLUME_EXPORT vtkFixedPointVolumeRayCastCompositeSCGOHelper : public vtkFixedPointVolumeRayCastHelper
+{
+public:
+  static vtkFixedPointVolumeRayCastCompositeSCGOHelper *New();
+  vtkTypeMacro(vtkFixedPointVolumeRayCastCompositeSCGOHelper,vtkFixedPointVolumeRayCastHelper);
+  void PrintSelf( ostream& os, vtkIndent indent );
+
+  virtual void  GenerateImage( int threadID,
+                               int threadCount,
+                               vtkVolume *vol,
+                               vtkFixedPointVolumeRayCastMapper *mapper);
+
+protected:
+  vtkFixedPointVolumeRayCastCompositeSCGOHelper();
+  ~vtkFixedPointVolumeRayCastCompositeSCGOHelper();
+
+private:
+  vtkFixedPointVolumeRayCastCompositeSCGOHelper(const vtkFixedPointVolumeRayCastCompositeSCGOHelper&);  // Not implemented.
+  void operator=(const vtkFixedPointVolumeRayCastCompositeSCGOHelper&);  // Not implemented.
+};
+
+#endif
+
+
